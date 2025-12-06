0dfdb3d6ad33fca854dbd0c05f14d5f849fdc2c0
xR-leo <leonidromaschov12@gmail.com>
2025-12-06 10:16:49 +0300

    Delete Sm_1.bat

--- Sm_1.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Mode changed successfully: Safe Mode (minimal).
-echo You need to restart your PC for Windows to boot in this mode.
-echo.
-bcdedit /set {current} safeboot minimal
-echo Press any key to return to the menu...
-set /p choice=" "
-exit /b
